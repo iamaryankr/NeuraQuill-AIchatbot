02c03fdc220f7b55b652fee2c9cb532650cbf039
iamaryankr <95643709+iamaryankr@users.noreply.github.com>
2025-06-07 00:18:53 +0530

    json

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,16 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
 
+// if in production, serve React build files
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
